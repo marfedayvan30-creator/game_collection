cbc22e3d587f33aed4eab269a00d9e2070b72460
LloYd Marcelino <lloydmarcelino@gmail.com>
2026-05-18 16:40:10 -0500

    Added various endpoints for users and games

--- game_collection/gaming/urls.py
+++ game_collection/gaming/urls.py
@@ -4,10 +4,25 @@ from .views import *
 
 urlpatterns = [
     path('test/', functions),
-    path('test-post/', simple_post_function),
 
     #Users
     ##GET
     path('users', return_all_users),
     path('users/<int:user_id>/games_played', return_all_games_played),
+    ##POST
+    path('users/create', create_user),
+    ##DELETE
+    path('users/delete/<int:user_id>', delete_user),
+    ##PATCH
+    path('users/update/<int:user_id>', update_user),
+
+    #Games
+    ##GET
+    path('games', return_all_games),
+    ##POST
+    path('games/add', add_game),
+    ##DELETE
+    path('games/delete/<int:game_id>', delete_game),
+    ##PATCH
+    path('games/update/<int:game_id>', patch_game),
 ]
--- game_collection/gaming/views.py
+++ game_collection/gaming/views.py
@@ -1,20 +1,39 @@
 from django.http import HttpResponse, JsonResponse
 from .models import *
 from django.shortcuts import get_object_or_404
+import json
 
 # Create your views here.
 def functions(request):
     return HttpResponse("Francis is gay, this is not a test!")
 
-def simple_post_function(request):
+#POST request to create a user
+def create_user(request):
     if request.method == 'POST':
-        decoded_data = request.body.decode('utf-8')
-        print(decoded_data)
-        return HttpResponse('Data was received')
+        data = json.loads(request.body)
+        user = Users.objects.create(
+            first_name = data['first_name'],
+            last_name = data['last_name'],
+            username = data['username'],
+            email = data['email'],
+            password = data['password']
+        )
+        return JsonResponse({
+            "id": user.id,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "username": user.username,
+            "email": user.email,
+            "password": user.password
+        })
     else:
         return HttpResponse('This is a POST only endpoint!', status=405)
-    
+
+#GET request to show all users    
 def return_all_users(request):
+    if request.method != 'GET':
+        return HttpResponse('This is a GET only endpoint', status=405)
+
     users = Users.objects.all()
     users_serialized = []
 
@@ -32,6 +51,7 @@ def return_all_users(request):
     print(users_serialized)
     return JsonResponse(users_serialized, safe=False)
 
+#GET request to show all the games played per user
 def return_all_games_played(request, user_id):
     user= get_object_or_404(Users, pk=user_id)
 
@@ -59,3 +79,135 @@ def return_all_games_played(request, user_id):
             "user_username": user.username,
             "games_played": played_serialized
                         })
+
+#DELETE request to remove a user using id
+def delete_user(request, user_id):
+    if request.method == 'DELETE':
+        user = get_object_or_404(Users, pk=user_id)
+
+        user.delete()
+
+        return HttpResponse(f'User {user_id} has been deleted!', status=200)
+    else:
+        return HttpResponse('This is an DELETE only endpoint!', status=405)
+
+#PATCH request to update user data    
+def update_user(request, user_id):
+    if request.method == 'PATCH':
+        user = get_object_or_404(Users, pk=user_id)
+        data = json.loads(request.body)
+
+        if 'first_name' in data:
+            user.first_name = data['first_name']
+        if 'last_name' in data:
+            user.last_name = data['last_name']
+        if 'username' in data:
+            user.username = data['username']
+        if 'email' in data:
+            user.email = data['email']
+        if 'password' in data:
+            user.password = data['password']
+        
+        user.save()
+        
+        return JsonResponse({
+            "id": user.id,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "username": user.username,
+            "email": user.email,
+            "password": user.password
+        })
+    else:
+        return HttpResponse('This is a PATCH only endpoint!', status=405)
+
+ #GET request to display all games   
+def return_all_games(request):
+    if request.method == 'GET':
+        games = Games.objects.all()
+        games_serialized = []
+
+        for game in games:
+            games_serialized.append(
+                {
+                    'id': game.id,
+                    'name': game.name,
+                    'genre': game.genre.name,
+                    'developer': game.developer.name,
+                    'release date': game.release_date
+
+                }
+            )
+        print(games_serialized)
+        return JsonResponse(games_serialized, safe=False)
+    else:
+        return HttpResponse('This is a GET only endpoint!', status=405)
+    
+#POST request to add new games
+def add_game(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        genre = get_object_or_404(Genres, name=data['genre'])
+        developer = get_object_or_404(Developers, name=data['developer'])
+
+        game = Games.objects.create(
+            name = data['name'],
+            genre = genre,
+            developer = developer,
+            release_date = data['release_date']
+        )
+
+        return JsonResponse({
+            'id': game.id,
+            'name': game.name,
+            'genre': game.genre.name,
+            'developer': game.developer.name,
+            'release_date': game.release_date
+        })
+    else:
+        return HttpResponse('This is a POST only endpoint', status=405)
+
+#DELETE request for deleting a game    
+def delete_game(request, game_id):
+    if request.method == 'DELETE':
+        game = get_object_or_404(Games, pk=game_id)
+        
+        game.delete()
+        
+        return HttpResponse(f'Game id: {game_id} has been deleted', status=200)
+    else:
+        return HttpResponse('This is a DELETE endpoint only!', status=405)
+    
+def patch_game(request, game_id):
+    if request.method == 'PATCH':
+        game = get_object_or_404(Games, pk=game_id)
+        data = json.loads(request.body)
+
+        if 'name' in data:
+            game.name = data['name']
+        if 'genre' in data:
+            genre = get_object_or_404(Genres, name=data['genre'])
+            game.genre = genre
+        if 'developer' in data:
+            developer = get_object_or_404(Developers, name=data['developer'])
+            game.developer = developer
+        if 'release_date' in data:
+            game.release_date = data['release_date']
+
+        game.save()
+
+        return JsonResponse({
+            'id': game.id,
+            'name': game.name,
+            'genre': game.genre.name,
+            'developer': game.developer.name,
+            'release_date': game.release_date
+        })
+    else:
+        return HttpResponse('This is a PATCH endpoint only!', status=405)
+
+
+
+
+
